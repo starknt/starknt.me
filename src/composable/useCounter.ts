93aceffabd7f1c51352d33c90949a2d50fb8f88c
starknt <1431880400@qq.com>
2022-08-03 10:11:20 +0800

    chore: rm test case

--- src/composable/useCounter.ts
+++
@@ -1,8 +0,0 @@
-export function useCounter(initivalue?: number) {
-  const count = ref(initivalue ?? 0)
-
-  const increment = () => count.value++
-  const decrement = () => count.value--
-
-  return { count, increment, decrement }
-}
--- test/App.spec.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/vue'
-import App from '~/App.vue'
-
-test('App component', () => {
-  const { getByText } = render(App)
-
-  expect(getByText('Vue Counter')).toBeDefined()
-})
