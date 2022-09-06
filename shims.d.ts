700e2385bf1715aff96e4dc39585dc669f13f426
starknt <1431880400@qq.com>
2022-09-06 21:21:17 +0800

    chore: lint and type tips

--- shims.d.ts
+++ shims.d.ts
@@ -1,5 +1,13 @@
+import 'vue-router'
+
 declare module '*.vue' {
     import { defineComponent } from 'vue'
     const Component: ReturnType<typeof defineComponent>
     export default Component
+}
+
+declare module 'vue-router' {
+    interface RouteMeta {
+        frontmatter: Record<string, string>
+    }
 }
